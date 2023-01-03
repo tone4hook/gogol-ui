7e737e82a8e8adfda219c4e2fd9d1a0031c697f4
tone4hook <tone4hook@users.noreply.github.com>
2023-01-03 07:53:27 +0000

    Deploying to gh-pages from  @ cc21ac9f781dca066a37506b81997e6d484ed054 🚀

--- src/styles/utils/createStyles.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Interpolation, Theme } from "@emotion/react";
-
-export const createStyles = <T extends { [key: string]: Interpolation<Theme> }>(
-    arg: T
-): T => arg;
